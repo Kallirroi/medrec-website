30d9e9597e7c4e868b5305a3a1284c4c33c0a89d
Kallirroi <kallirroi.retzepi@gmail.com>
2017-10-09 14:05:09 -0400

    added images and text

--- app/components/Team/Team.js
+++ app/components/Team/Team.js
@@ -3,17 +3,29 @@ import React from 'react';
 const Team = function() {
 	return (
 		<div className="team-wrapper">
+			<div className="team-card"> 
+				<img width={'140px'}  src={'/images/Andy.jpg'} alt={'Andy Lipman'} />
+				<span className="team-info"> Andy Lipman <br/> Principal Investigator <br/> <span className="team-info-contact"> contact </span> </span>
+			</div>
 			<div className="team-card">
-				<img width={'140px'}  src={'/images/ariel.jpg'} alt={'Ariel Ekblaw'} />
-				<span className="team-info"> Ariel Ekblaw <br/> <span className="team-info-contact"> contact </span> </span>
+				<img width={'140px'}  src={'/images/Ariel.jpg'} alt={'Ariel Ekblaw'} />
+				<span className="team-info"> Ariel Ekblaw <br/> PhD candidate, MIT Media Lab  <br/> <span className="team-info-contact"> contact </span> </span>
 			</div>
+			<div className="team-card">
+				<img width={'140px'}  src={'/images/Agnes.jpg'} alt={'Agnes Cameron'} />
+				<span className="team-info"> Agnes Cameron <br/> MA candidate, MIT Media Lab  <br/> <span className="team-info-contact"> contact </span> </span>
+			</div>				
+			<div className="team-card">
+				<img width={'140px'}  src={'/images/Britney.jpg'} alt={'Britney Johnson'} />
+				<span className="team-info"> Britney Johnson <br/> MA candidate, MIT Media Lab  <br/> <span className="team-info-contact"> contact </span> </span>
+			</div>				
+			<div className="team-card">
+				<img width={'140px'}  src={'/images/Kalli.jpg'} alt={'Kalli Retzepi'} />
+				<span className="team-info"> Kalli Retzepi <br/> MA candidate, MIT Media Lab  <br/> <span className="team-info-contact"> contact </span> </span>
+			</div>			
 			<div className="team-card"> 
-				<img width={'140px'}  src={'/images/safa.jpg'} alt={'Asaph Azaria'} />
-				<span className="team-info"> Asaph Azaria <br/> <span className="team-info-contact"> contact </span> </span>
-			</div>
-			<div className="team-card"> 
-				<img width={'140px'}  src={'/images/andy.jpeg'} alt={'Andy Lipman'} />
-				<span className="team-info"> Andy Lipman <br/> <span className="team-info-contact"> contact </span> </span>
+				<img width={'140px'}  src={'/images/Nchinda.jpg'} alt={'Nchinda Nchinda'} />
+				<span className="team-info"> Nchinda Nchinda <br/> MA candidate, MIT  <br/>  <span className="team-info-contact"> contact </span> </span>
 			</div>
 		</div>
 	);
