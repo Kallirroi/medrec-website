2484806a4027748c81d5bc032cabded697671021
Kallirroi <kallirroi.retzepi@gmail.com>
2018-03-06 20:26:15 -0500

    ga

--- app/indexProd.js
+++ app/indexProd.js
@@ -26,6 +26,13 @@ export default function(locals) {
 		${helmet.title.toString().replace(regexp, '')}
 		${helmet.meta.toString().replace(regexp, '')}
 		${helmet.link.toString().replace(regexp, '')}
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115316575-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+		  gtag('config', 'UA-115316575-1');
+		</script>
 	</head>
 	<body>
 		${app}
